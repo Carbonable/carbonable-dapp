f43df43a5b24f7945fc17fb6dcfc39efa78b04c3
rmzlb <ramzi.laieb@gmail.com>
2022-12-04 12:26:05 +0100

    quest

--- app/components/NavMenu/links.ts
+++ app/components/NavMenu/links.ts
@@ -34,5 +34,11 @@ export const links = [
         label: 'Impact',
         isOpen: false,
         outsideLink: false
+    },
+    {
+        href: '/quest',
+        label: 'quest',
+        isOpen: true,
+        outsideLink: false
     }
 ]
