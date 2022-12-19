abfa2a3cbd2f1fb0a1e901ab07fede33c062cfc6
François Schuers <francois.schuers@gmail.com>
2022-12-20 00:03:56 +0100

    feat: remove filters - change countdown color - align center on mobile

--- app/routes/__index/farming/index.tsx
+++ app/routes/__index/farming/index.tsx
@@ -99,22 +99,22 @@ export default function FarmingIndex() {
     return (
         <div className="mx-auto md:mt-12 lg:mt-6 max-w-7xl">
             <div className="relative w-11/12 mx-auto border border-neutral-700 bg-launchpad-header rounded-3xl p-8 flex items-start justify-start flex-wrap md:px-10">
-                <div className="flex items-start justify-start flex-wrap w-full md:w-5/12">
-                    <div className="font-trash text-lg uppercase w-full lg:text-xl">My farming assets</div>
+                <div className="flex items-start justify-center flex-wrap w-full md:w-5/12 md:justify-start">
+                    <div className="font-trash text-lg uppercase w-full lg:text-xl text-center md:text-left">My farming assets</div>
                     {status === 'connected' && <div className="font-trash text-4xl uppercase mt-4 text-neutral-300">$2500</div>}
                     {status === 'disconnected' && <div className="font-trash text-xl uppercase mt-4">
                         <ConnectButton />
                         </div>}
                 </div>
-                <div className="flex items-start justify-start flex-wrap w-full mt-8 md:w-5/12 md:mt-0">
-                    <div className="font-trash text-lg uppercase w-full lg:text-xl">End of current cycle</div>
+                <div className="flex items-start justify-center flex-wrap w-full mt-8 md:w-5/12 md:mt-0 md:justify-start">
+                    <div className="font-trash text-lg uppercase w-full lg:text-xl text-center md:text-left">End of current cycle</div>
                     <Countdown countdown={countdown} />
                 </div>
                 <img src="/assets/images/common/logo-grey.svg" alt="Carbonable logo grey" className="absolute bottom-0 right-12 w-[100px] xl:right-20 lg:w-[110px]" />
             </div>
             <div className="relative w-11/12 mx-auto mt-12 lg:mt-12 xl:mt-16 mb-12 md:pl-6">
                 <div className="uppercase font-trash text-lg text-center md:text-left md:pl-1 lg:text-xl">Farming Projects</div>
-                <div className="flex items-center justify-start mt-4">
+                <div className="hidden items-center justify-start mt-4">
                     {
                         filterButtons.map((button, index) => {
                             return <FilterButton onClick={() => {handleclick(button.filter)}} active={button.active} key={index} disabled={button.isDisabled}>{button.filter}</FilterButton>
