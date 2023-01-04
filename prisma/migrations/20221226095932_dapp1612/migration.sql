a3465e4a34894e624e4fe6b92630c453990aab72
François Schuers <francois.schuers@gmail.com>
2023-01-04 09:48:23 +0100

    fix: add missing database migration

--- prisma/migrations/20221226095932_dapp1612/migration.sql
+++ prisma/migrations/20221226095932_dapp1612/migration.sql
@@ -1,13 +1,2 @@
--- AlterTable
-ALTER TABLE "Project" ADD COLUMN     "name" TEXT NOT NULL DEFAULT '';
-
--- CreateTable
-CREATE TABLE "Snapshot" (
-    "id" TEXT NOT NULL,
-    "snapshotDate" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Snapshot_pkey" PRIMARY KEY ("id")
-);
-
 -- RenameIndex
 ALTER INDEX "Badge_tokenid_key" RENAME TO "Badges_token_id_key";
---
+++ prisma/migrations/20230104084550_badge_whitelist/migration.sql
@@ -0,0 +1,35 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Badges` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Badges";
+
+-- CreateTable
+CREATE TABLE "Badge" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "subtitle" TEXT NOT NULL,
+    "mintable" BOOLEAN NOT NULL DEFAULT false,
+    "token_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Badge_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BadgeWhitelist" (
+    "id" TEXT NOT NULL,
+    "whitelist" JSONB NOT NULL,
+
+    CONSTRAINT "BadgeWhitelist_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Badge_token_id_key" ON "Badge"("token_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "BadgeWhitelist_whitelist_key" ON "BadgeWhitelist"("whitelist");
