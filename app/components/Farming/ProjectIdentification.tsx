bbcd5651a18cafd8ea957408b371e15a30c77268
François Schuers <francois.schuers@gmail.com>
2023-02-16 19:41:41 +0100

    Start new design

--- app/components/Farming/ProjectIdentification.tsx
+++ app/components/Farming/ProjectIdentification.tsx
@@ -1,19 +1,7 @@
 import type { Project } from "@prisma/client";
 import { IPFS_GATEWAY } from "~/utils/links";
-import Undeposited from "./Undeposited";
 
-export default function ProjectIdentification({project, deposited, status, displayDeposited}: {project: Project, deposited: boolean | undefined, status: string, displayDeposited: boolean}) {
-    if (displayDeposited) return (
-        <div className="flex items-center justify-start w-full">
-            <div className="w-2/12">
-                <img src={IPFS_GATEWAY + project.imageIpfs} alt={`${project.slug} NFT card`} className="w-12 rounded-[8.8%] lg:w-16 xl:w-18" />
-            </div>
-            <div className="px-4 font-inter text-neutral-100 text-lg lg:text-2xl font-bold w-6/12 uppercase">{project.name}</div>
-            <div className="w-4/12 flex justify-end">
-                {deposited && status === 'connected' && <Undeposited /> }
-            </div>
-        </div>
-    )
+export default function ProjectIdentification({project}: {project: Project}) {
     return (
         <div className="flex items-center justify-start w-full px-4 md:px-0">
             <div className="w-3/12">
--- app/routes/__index/farming/index.tsx
+++ app/routes/__index/farming/index.tsx
@@ -1,12 +1,7 @@
-import type { Project, Snapshot } from "@prisma/client";
+import type { Project } from "@prisma/client";
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { NavLink, useLoaderData } from "@remix-run/react";
-import { useAccount } from "@starknet-react/core";
-import moment from "moment";
-import { useState } from "react";
-import ConnectButton from "~/components/Buttons/ConnectButton";
-import Countdown from "~/components/Countdown/Countdown";
 import FarmingCard from "~/components/Farming/FarmingCard";
 import FilterButton from "~/components/Filters/FilterButton";
 import { userPrefs } from "~/cookie";
@@ -16,17 +11,6 @@ export const loader: LoaderFunction = async ({
     request, 
   }) => {
     try {
-        const nextSnapshot = await db.snapshot.findMany({
-            where: {
-                snapshotDate: {
-                    gte: new Date()
-                  }
-            },
-            orderBy: {
-                snapshotDate: 'asc'
-            }
-        });
-
         const cookieHeader = request.headers.get("Cookie");
         const cookie = (await userPrefs.parse(cookieHeader)) || {};
 
@@ -46,7 +30,7 @@ export const loader: LoaderFunction = async ({
                 saleDate: 'desc',
               }
             ]});
-        return json([nextSnapshot[0], allProjects]);
+        return json([allProjects]);
     } catch (e) {
         
         return json({});
@@ -55,14 +39,7 @@ export const loader: LoaderFunction = async ({
 
 export default function FarmingIndex() {
     const loaderData = useLoaderData();
-    const snapshot: Snapshot = loaderData[0];
-    const projects: Project[] = loaderData[1];
-
-    const { status } = useAccount();
-    let now = moment();
-
-    const snapshotDate = moment(snapshot?.snapshotDate || now);
-    const[countdown, setCountdown] = useState(moment.duration(snapshotDate.diff(now)));
+    const projects: Project[] = loaderData[0];
 
     const filterButtons = [
         {
@@ -87,48 +64,39 @@ export default function FarmingIndex() {
         }
     ];
 
-    setInterval(() => {
-        now = moment();
-        setCountdown(moment.duration(snapshotDate.diff(now)));
-    }, 1000)
-
     const handleclick = (filter: string) => {
         console.log(filter);
     }
 
     return (
         <div className="mx-auto md:mt-12 lg:mt-6 max-w-7xl">
-            <div className="relative w-11/12 mx-auto border border-neutral-700 bg-farming bg-cover bg-[50%_22%] rounded-3xl p-8 flex items-start justify-start flex-wrap md:px-10">
+            <div className="relative w-11/12 mx-auto border border-neutral-700 bg-farming bg-cover bg-[50%_22%] rounded-3xl p-8 flex items-start justify-start flex-wrap md:p-10 lg:p-12">
                 <div className="flex items-start justify-center flex-wrap w-full md:w-5/12 md:justify-start">
-                    <div className="font-trash text-lg uppercase w-full lg:text-xl text-center md:text-left">My farming assets</div>
-                    {status === 'connected' && <div className="font-trash text-4xl uppercase mt-4 text-neutral-300">$2500</div>}
-                    {status === 'disconnected' && <div className="font-trash text-xl uppercase mt-4">
-                        <ConnectButton />
-                        </div>}
+                    <div className="font-trash uppercase w-full lg:text-lg text-center md:text-left">My farming assets</div>
+                    <div className="font-americana text-4xl mt-4 text-neutral-200 font-extrabold">$0</div>
                 </div>
                 <div className="flex items-start justify-center flex-wrap w-full mt-8 md:w-5/12 md:mt-0 md:justify-start">
-                    <div className="font-trash text-lg uppercase w-full lg:text-xl text-center md:text-left">End of current cycle</div>
-                    <Countdown countdown={countdown} />
+                    <div className="font-trash uppercase w-full lg:text-lg text-center md:text-left">Claimable assets</div>
+                    <div className="font-americana text-4xl mt-4 text-neutral-200 font-extrabold flex items-center">$0<span className="font-inter font-extralight text-lg px-4">|</span>t0</div>
                 </div>
                 <img src="/assets/images/common/logo-grey.svg" alt="Carbonable logo grey" className="absolute bottom-0 right-12 w-[100px] xl:right-20 lg:w-[110px]" />
             </div>
             <div className="relative w-11/12 mx-auto mt-12 lg:mt-12 xl:mt-16 mb-12 md:pl-6">
                 <div className="uppercase font-trash text-lg text-center md:text-left md:pl-1 lg:text-xl">Farming Projects</div>
-                <div className="hidden items-center justify-start mt-4">
+                <div className="items-center justify-start mt-4">
                     {
                         filterButtons.map((button, index) => {
                             return <FilterButton onClick={() => {handleclick(button.filter)}} active={button.active} key={index} disabled={button.isDisabled}>{button.filter}</FilterButton>
                         })
                     }
                 </div>
-                <div className="flex flex-wrap items-center justify-start mt-4 md:justify-between">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-start mt-4 gap-4">
                 {
                     projects.map((project, index) => {
                         return (
-                            <NavLink key={index} to={`/farming/${project.slug}`} className="w-full md:w-[48%]">
+                            <NavLink key={index} to={`/farming/${project.slug}`} className="w-full">
                                 <FarmingCard project={project} />
                             </NavLink>
-                            
                         )})
                     }
                 </div>
