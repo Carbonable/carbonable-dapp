10c199cd7ed8608d5a373dbb208e8c3f8b5730ee
François Schuers <francois.schuers@gmail.com>
2023-03-30 11:21:32 +0200

    feat: add argent webwallet

--- app/hooks/project.ts
+++ app/hooks/project.ts
@@ -1,20 +1,17 @@
-import { useContract, useStarknetCall } from '@starknet-react/core';
+import { useContractRead } from '@starknet-react/core';
 import type { Abi } from 'starknet';
 
 import ProjectAbiTestnet from '../abi/testnet/CarbonableProject_abi.json';
 import ProjectAbiTestnet2 from '../abi/testnet2/CarbonableProject_abi.json';
 import ProjectAbiMainnet from '../abi/mainnet/CarbonableProject_abi.json';
 
-export function useProjectContract(contractAddress: string | undefined, network: string) {
+export function useProjectContract(network: string) {
   let abi = ProjectAbiMainnet;
   
   if (network === "testnet") { abi = ProjectAbiTestnet }
   if (network === "testnet2") { abi = ProjectAbiTestnet2 }
   
-  return useContract({
-    abi: abi as Abi,
-    address: contractAddress,
-  })
+  return abi as Abi;
 }
 
 /**
@@ -24,9 +21,9 @@ export function useProjectContract(contractAddress: string | undefined, network:
  * @returns { any } Project Total Supply
  */
  export function useProjectTotalSupply(contractAddress: string, network: string): any {
-  const { contract: project } = useProjectContract(contractAddress, network);
-  const { data, loading, error, refresh } = useStarknetCall({ contract: project, method: 'totalSupply', args: [] });
+  const abi = useProjectContract(network);
+  const { data, error } = useContractRead({ abi, address: contractAddress, functionName: 'totalSupply', args: [] });
   const totalSupply = data ? data[0] : undefined;
   
-  return { projectTotalSupply: totalSupply?.low.toString(), errorProjectTotalSupply: error, loadingProjectTotalSupply: loading, refreshProjectTotalSupply: refresh }; 
+  return { projectTotalSupply: totalSupply?.low.toString(), errorProjectTotalSupply: error }; 
 }
