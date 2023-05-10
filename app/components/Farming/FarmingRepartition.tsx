5de9adf6c77c7b006609b499106064403a0fcd5c
François Schuers <francois.schuers@gmail.com>
2023-05-10 23:56:54 +0200

    fix: deposit and withdraw calls

--- app/components/Farming/FarmingRepartition.tsx
+++ app/components/Farming/FarmingRepartition.tsx
@@ -6,8 +6,8 @@ export default function FarmingRepartition({yieldAmount, offsetAmount}: {yieldAm
     return (
         <div className="relative w-full">
             <div className="flex items-center justify-between px-4 text-xs uppercase lg:text-base">
-                <div className="text-green font-inter">YIELD {yieldPercentage}%</div>
-                <div className="text-blue font-inter">OFFSET {offsetPercentage}%</div>
+                <div className="text-green font-inter">YIELD {yieldPercentage.toFixed(1)}%</div>
+                <div className="text-blue font-inter">OFFSET {offsetPercentage.toFixed(1)}%</div>
             </div>
             {total > 0 && <div className="rounded-full h-2 w-full mt-3" style={{background: `linear-gradient(to right, #0AF2AD, ${yieldPercentage}%, #9EBAF0)`, backgroundImage: "rgba(0, 0, 0, 0.3)" }}></div>}
             {total === 0 && <div className="rounded-full h-2 w-full mt-3 bg-neutral-400"></div>}
