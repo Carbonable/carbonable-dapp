68bf98871f9c1055e13987fd8dc2145c5a0876f8
François Schuers <francois.schuers@gmail.com>
2023-05-23 15:55:24 +0200

    feat: use data from indexer

--- app/routes/__index/launchpad/index.tsx
+++ app/routes/__index/launchpad/index.tsx
@@ -2,10 +2,32 @@ import type { LoaderFunction, V2_MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { NavLink, useLoaderData } from "@remix-run/react";
 import { db } from "~/utils/db.server";
-import type { Project } from "@prisma/client";
 
 import { userPrefs } from "~/cookie";
 import LaunchpadCard from "~/components/Project/Overview/ProjectCard";
+
+export interface ProjectProps {
+    id: string,
+    slug: string,
+    name: string,
+    address: string,
+    uri: any
+}
+
+export interface LaunchpadProps {
+    image: string | null,
+    is_ready: boolean,
+    is_sold_out: boolean,
+    minter_contract: any,
+    public_sale_open: boolean,
+    sale_date: Date | string | null,
+    whitelisted_sale_open: boolean,
+}
+
+export interface LaunchpadLoaderData {
+    project: ProjectProps,
+    launchpad: LaunchpadProps
+}
 
 
 export const loader: LoaderFunction = async ({
@@ -52,9 +74,7 @@ export const meta: V2_MetaFunction = () => {
 };
 
 export default function Launchpad() {
-    const projects = useLoaderData<unknown>() as Project[];
-    console.log(projects)
-
+    const projects = useLoaderData().data as LaunchpadLoaderData[];
     return (
         <div className="mx-auto md:mt-12 lg:mt-6 max-w-7xl">
             <div className="relative w-11/12 mx-auto border border-neutral-700 bg-launchpad-header rounded-3xl p-4 flex items-center justify-center flex-wrap md:px-10">
@@ -71,18 +91,23 @@ export default function Launchpad() {
             <div className="relative w-11/12 mx-auto mt-12 lg:mt-12 xl:mt-16 mb-12 md:pl-6">
                 <div className="uppercase font-inter text-bold text-lg text-center md:text-left md:pl-1 2xl:text-xl">Projects to finance</div>
                 <div className="flex flex-wrap items-start justify-center mt-2 md:justify-start">
-                    {projects.map((project: Project) => (     
-                        <div key={project.slug} className="w-full my-2 mx-2 md:ml-0 md:mr-4 md:w-[31%] lg:w-[31%]">
-                            { project.contentReady && 
-                                <NavLink to={`/launchpad/${project.slug}`}>
+                    {projects && projects.map((project: LaunchpadLoaderData) => (     
+                        <div key={project.project.slug} className="w-full my-2 mx-2 md:ml-0 md:mr-4 md:w-[31%] lg:w-[31%]">
+                            { project.launchpad.is_ready && 
+                                <NavLink to={`/launchpad/${project.project.slug}`}>
                                     <LaunchpadCard {...project} />
                                 </NavLink>
                             }
-                            { project.contentReady === false && 
+                            { project.launchpad.is_ready === false && 
                                 <LaunchpadCard {...project} />
                             }
                         </div>
                     ))}
+                    { (projects === undefined || projects.length === 0) &&
+                        <div className="text-xl px-2 text-neutral-300">
+                            No projects available right now
+                        </div>
+                    }
                 </div>
             </div>
         </div>
