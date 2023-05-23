1247f4a89c014c684627272349b76f4467dcff42
François Schuers <francois.schuers@gmail.com>
2023-05-23 15:55:24 +0200

    feat: use data from indexer

--- app/components/Project/Overview/ProjectCard.tsx
+++ app/components/Project/Overview/ProjectCard.tsx
@@ -1,35 +1,42 @@
-import type { Project } from "@prisma/client";
 import moment from "moment";
 import { useEffect, useState } from "react";
 import { PlusIconBlack } from "~/components/Icons/PlusIcon";
-import { useSoldout } from "~/hooks/minter";
-import { IPFS_GATEWAY } from "~/utils/links";
+import type { LaunchpadLoaderData } from "~/routes/__index/launchpad";
+import { getImageUrlFromMetadata } from "~/utils/utils";
 
-export default function LaunchpadCard({slug, saleDate, minterContract, networkId, imageIpfs, whitelistedSaleOpen, publicSaleOpen, isSoldout}: Project) {
-    const { soldout } = useSoldout(minterContract, networkId);
+export default function LaunchpadCard(project: LaunchpadLoaderData) {
     const [saleIsOpen, setSaleIsOpen] = useState(false);
-    const [projectIsSoldout, setProjectIsSoldout] = useState(false);
+    const [imageSrc, setImageSrc] = useState<string>("/assets/images/backgrounds/bg-farming-card.png");
 
     useEffect(() => {
-        setProjectIsSoldout(soldout || isSoldout);
-    }, [soldout, isSoldout]);
+        if (project.project.uri.uri) {
+            getImageUrlFromMetadata(project.project.uri.uri).then((url) => {
+                setImageSrc(url);
+            });
+        }
+    }, [project.project.uri.uri]);
 
     useEffect(() => {
-        setSaleIsOpen((publicSaleOpen || whitelistedSaleOpen) ? true : false);
-    }, [whitelistedSaleOpen, publicSaleOpen]);
+        setSaleIsOpen((project.launchpad.public_sale_open || project.launchpad.whitelisted_sale_open) ? true : false);
+    }, [project.launchpad.public_sale_open, project.launchpad.whitelisted_sale_open]);
 
     return (
-        <div className="relative">
-            <img src={IPFS_GATEWAY + imageIpfs} alt={`${slug}  NFT card`} className="w-full rounded-[8.8%]" />
-            { (projectIsSoldout || saleIsOpen === false) && 
+        <div className="relative  min-h-[320px]">
+            <img src={imageSrc} alt={`${project.project.slug} NFT card`} className="w-full rounded-[8.8%]" />
+            { (project.launchpad.is_sold_out || saleIsOpen === false) && 
                 <div className="absolute top-0 left-0 bg-white/40 w-full h-[100%] rounded-[10%]"></div>
             }
-            { projectIsSoldout && 
+            { project.launchpad.is_sold_out && 
                 <div className="absolute uppercase font-inter font-bold bg-beaige text-black top-[6%] left-[6%] py-1 px-2 text-[8px] md:text-xs lg:px-3 rounded-lg">Soldout</div>
             }
-            { saleIsOpen === false && moment(saleDate).isAfter(moment(new Date()))  && 
+            { saleIsOpen === false && project.launchpad.sale_date === null && project.launchpad.is_sold_out === false &&
                 <div className="absolute flex items-center uppercase font-inter font-bold bg-green-blue text-black top-[6%] left-[6%] py-1 px-2 text-[8px] md:text-xs lg:px-3 rounded-lg">
-                    <span>Coming soon</span>&nbsp;<PlusIconBlack className="w-2"></PlusIconBlack>&nbsp;<span>{moment(saleDate).format("MM.DD.YYYY").toString()}</span>
+                    <span>Coming soon</span>
+                </div>
+            }
+            { saleIsOpen === false && moment(project.launchpad.sale_date).isAfter(moment(new Date())) && project.launchpad.is_sold_out === false &&
+                <div className="absolute flex items-center uppercase font-inter font-bold bg-green-blue text-black top-[6%] left-[6%] py-1 px-2 text-[8px] md:text-xs lg:px-3 rounded-lg">
+                    <span>Coming soon</span>&nbsp;<PlusIconBlack className="w-2"></PlusIconBlack>&nbsp;<span>{moment(project.launchpad.sale_date).format("MM.DD.YYYY").toString()}</span>
                 </div>
             }
         </div>
