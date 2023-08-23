88fbeaf17cea59f2008095a8a5a9299d32225371
François Schuers <francois.schuers@gmail.com>
2023-08-23 21:41:26 +0200

    feat: indicate if the connector must be downloaded

--- app/components/Connection/ConnectDialog.tsx
+++ app/components/Connection/ConnectDialog.tsx
@@ -59,11 +59,11 @@ export default function ConnectDialog({ isOpen, setIsOpen }: {isOpen: boolean, s
                                 <div key={wallet.id + "_modal"}>
                                     {wallet.available() && <div className="p-4 my-2 flex items-center justify-start cursor-pointer rounded-2xl bg-opacityLight-5 hover:bg-opacityLight-10 w-full" onClick={() => handleClick(wallet)}>
                                         <img className="w-5 h-5 mr-3" src={wallet.id === 'argentWebWallet' ? '/assets/images/common/argentX.svg' : `/assets/images/common/${wallet.id}.svg`} alt={`Connect with ${wallet.id}`} />
-                                        <div className="uppercase font-inter mt-2">{wallet.id === 'argentWebWallet' ? 'Argent Web Wallet' : wallet.id}</div>
+                                        <div className="uppercase font-inter">{wallet.id === 'argentWebWallet' ? 'Argent Web Wallet' : wallet.id}</div>
                                     </div>}
-                                    {!wallet.available() && <a className="p-4 my-2 flex items-center justify-start cursor-pointer rounded-2xl bg-opacityLight-5 hover:bg-opacityLight-10 w-full" href={wallet.id === 'argentX' ? 'https://www.argent.xyz/argent-x/' : 'https://braavos.app/'} rel="noreferrer" target="_blank">
+                                    {!wallet.available() && <a className="p-4 my-2 flex items-center justify-start cursor-pointer rounded-2xl bg-opacityLight-5 hover:bg-opacityLight-10 w-full opacity-40 hover:opacity-100" href={wallet.id === 'argentX' ? 'https://www.argent.xyz/argent-x/' : 'https://braavos.app/'} rel="noreferrer" target="_blank">
                                         <img className="w-5 h-5 mr-3" src={wallet.id === 'argentWebWallet' ? '/assets/images/common/argentX.svg' : `/assets/images/common/${wallet.id}.svg`} alt={`Connect with ${wallet.id}`} />
-                                        <div className="uppercase font-inter mt-2">{wallet.id === 'argentWebWallet' ? 'Argent Web Wallet' : wallet.id}</div>
+                                        <div className="uppercase font-inter">{wallet.id === 'argentWebWallet' ? 'Argent Web Wallet' : wallet.id} <span className='text-neutral-300 text-sm ml-1'>(download)</span></div>
                                     </a>}
                                 </div>
                             ))}
