f72199e50a26a7fc8b317875afc09da765f60d82
François Schuers <francois.schuers@gmail.com>
2024-03-01 11:04:12 +0100

    feat: highlight current user in the leaderboard

--- app/components/Common/Table/RankingLine.tsx
+++ app/components/Common/Table/RankingLine.tsx
@@ -1,10 +1,13 @@
+import { useAccount } from "@starknet-react/core";
 import { type LeaderboardLineData } from "~/graphql/__generated__/graphql";
 import { minifyAddressOrStarknetId } from "~/utils/utils";
 
 export function RankingLine({ index, data, address }: { index: number, data: LeaderboardLineData, address: string }) {
+    const { address: walletAddress } = useAccount();
+    console.log(address, walletAddress)
     return (
-        <tr className="h-[36px] first:rounded-bl-xl last:rounded-br-xl">
-            <td className="px-4 border-r border-b border-opacityLight-10 first:border-l sticky left-0 z-10 bg-neutral-800 w-[240px]">
+        <tr className={`h-[36px] first:rounded-bl-xl last:rounded-br-xl ${address === walletAddress ? "bg-neutral-700" : ""}`}>
+            <td className={`px-4 border-r border-b border-opacityLight-10 first:border-l sticky left-0 z-10 ${address === walletAddress ? "bg-neutral-700" : "bg-neutral-800"} w-[240px]`}>
                 <div className="flex items-center">
                     <div className={`text-neutral-200 font-light ${index < 100 ? 'min-w-[14px]' : 'min-w-[22px]' }`}>{data.position}</div>
                     <div className="ml-1 mr-2 min-w-[28px]">
