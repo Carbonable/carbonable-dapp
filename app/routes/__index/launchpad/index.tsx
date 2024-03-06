2549fd48737eff82200330222c148c88516b873d
François Schuers <francois.schuers@gmail.com>
2024-03-06 15:27:56 +0100

    feat: change launchpad header

--- app/routes/__index/launchpad/index.tsx
+++ app/routes/__index/launchpad/index.tsx
@@ -1,8 +1,7 @@
 import type { LoaderFunction, V2_MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { NavLink, useLoaderData } from "@remix-run/react";
-import { TransparentLinkButton } from "~/components/Buttons/LinkButton";
-import ProjectStatus from "~/components/Common/ProjectStatus";
+import NextSale from "~/components/Launchpad/NextSale";
 import LaunchpadCard from "~/components/Launchpad/Overview/ProjectCard";
 import { type LaunchpadLoaderData } from "~/types/project";
 
@@ -43,7 +42,7 @@ export default function Launchpad() {
 
     return (
         <div className="mx-auto md:mt-12 lg:mt-4 max-w-7xl">
-            <LaunchpadHeader displayHeader={displayHeader} />
+            <NextSale displayHeader={displayHeader} />
             <div className="relative w-11/12 mx-auto mt-12 lg:mt-12 xl:mt-16 mb-12 md:pl-6">
                 <div className="uppercase font-inter text-bold text-lg text-center md:text-left md:pl-1 2xl:text-xl">Projects to finance</div>
                 <div className="flex flex-wrap items-start justify-center mt-2 md:justify-start">
@@ -64,44 +63,6 @@ export default function Launchpad() {
                             No projects available right now
                         </div>
                     }
-                </div>
-            </div>
-        </div>
-    )
-}
-
-function LaunchpadHeader({ displayHeader }: { displayHeader: boolean }) {
-
-    if (!displayHeader) return null;
-
-    return (
-        <div className="relative w-full md:w-11/12 mx-auto bg-karathuru rounded-lg md:px-10 bg-no-repeat bg-cover bg-center min-h-[360px] border-4 border-neutral-700">
-            <div className="absolute bottom-6 md:bottom-12 left-0 w-full">
-                <div className="flex justify-between w-full items-end px-8 flex-wrap">
-                    <div className="text-left w-full md:w-1/2">
-                        <div className="text-3xl font-bold">Karathuru Project</div>
-                        <div className="text-sm font-medium flex items-center mt-1">
-                            By 
-                            <div className="text-neutral-200 font-normal ml-1">Worldview International Foundation</div>
-                            <div className="ml-1">
-                                <img src="/assets/images/common/verified-icon.svg" alt="Verified Icon" className="h-4 w-4" />
-                            </div>
-                        </div>
-                        <div className="text-sm font-medium ">
-                            Pre-Register to access this exclusive funding opportunity
-                        </div>
-                        <div className="mt-8 flex justify-between">
-                            <div className="text-left">
-                                <TransparentLinkButton  href="https://register.carbonable.io">Pre-register</TransparentLinkButton>
-                            </div>
-                            <div className="text-right md:hidden">
-                                <ProjectStatus />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="text-right w-full md:w-1/2 mt-8 md:mt-0 hidden md:flex md:justify-end items-end">
-                        <ProjectStatus />
-                    </div>
                 </div>
             </div>
         </div>
