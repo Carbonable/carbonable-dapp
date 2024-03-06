6654084c54dfa7525801e161ba414f3564c6be30
François Schuers <francois.schuers@gmail.com>
2024-03-06 15:48:11 +0100

    fix: add global link

--- app/components/Common/ProjectStatus.tsx
+++ app/components/Common/ProjectStatus.tsx
@@ -3,7 +3,7 @@ export default function ProjectStatus() {
         <div className={`w-fit flex items-center justify-center rounded-full px-3 py-[2px] font-inter font-light text-sm text-blue-light bg-opacityDark-40 border border-blue-light/50 cursor-default`}>
             <div className="flex items-center">
                 <span className="h-[7px] w-[7px] rounded-full bg-blue-light mr-2"></span>
-                Upcoming
+                Upcoming - March 26th
             </div>
         </div>
     )
--- app/components/Launchpad/NextSale.tsx
+++ app/components/Launchpad/NextSale.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "@remix-run/react";
 import { TransparentInsideLinkButton } from "../Buttons/LinkButton";
 import ProjectStatus from "../Common/ProjectStatus";
 
@@ -5,35 +6,35 @@ export default function NextSale({ displayHeader }: { displayHeader: boolean }) 
     if (!displayHeader) return null;
 
     return (
-        <div className="relative w-full md:w-11/12 mx-auto bg-karathuru rounded-lg md:px-10 bg-no-repeat bg-cover bg-center min-h-[360px] border-4 border-neutral-700">
-            <div className="absolute bottom-6 md:bottom-12 left-0 w-full">
-                <div className="flex justify-between w-full items-end px-8 flex-wrap">
-                    <div className="text-left w-full md:w-1/2">
-                        <div className="text-3xl font-bold">Karathuru Project</div>
-                        <div className="text-sm font-medium flex items-center mt-1">
-                            By 
-                            <div className="text-neutral-200 font-normal ml-1">Worldview International Foundation</div>
-                            <div className="ml-1">
-                                <img src="/assets/images/common/verified-icon.svg" alt="Verified Icon" className="h-4 w-4" />
+        <NavLink to='/launchpad/mangroves-regeneration-karathuru-myanmar'>
+            <div className="relative w-full md:w-11/12 mx-auto bg-karathuru rounded-lg md:px-10 bg-no-repeat bg-cover bg-center min-h-[360px] border-4 border-neutral-700">
+                <div className="absolute bottom-6 md:bottom-12 left-0 w-full">
+                    <div className="flex w-full items-end px-8 flex-wrap">
+                        <div className="text-left w-full">
+                            <div className="text-3xl font-bold">Karathuru Project</div>
+                            <div className="text-sm font-medium flex items-center mt-1">
+                                By 
+                                <div className="text-neutral-200 font-normal ml-1">Worldview International Foundation</div>
+                                <div className="ml-1">
+                                    <img src="/assets/images/common/verified-icon.svg" alt="Verified Icon" className="h-4 w-4" />
+                                </div>
+                            </div>
+                            <div className="text-sm font-medium ">
+                                Pre-Register to access this exclusive funding opportunity
+                            </div>
+                            <div className="mt-4 flex justify-between flex-wrap items-center">
+                                <div className="text-left w-full md:w-fit">
+                                    <ProjectStatus />
+                                </div>
+                                <div className="w-full mt-8 text-left md:w-fit md:flex-grow md:mt-0 md:text-right">
+                                    <TransparentInsideLinkButton href="/launchpad/mangroves-regeneration-karathuru-myanmar">View Project &gt;</TransparentInsideLinkButton>
+                                </div>
                             </div>
                         </div>
-                        <div className="text-sm font-medium ">
-                            Pre-Register to access this exclusive funding opportunity
-                        </div>
-                        <div className="mt-8 flex justify-between">
-                            <div className="text-left">
-                                <TransparentInsideLinkButton href="/launchpad/mangroves-regeneration-karathuru-myanmar">View Project</TransparentInsideLinkButton>
-                            </div>
-                            <div className="text-right md:hidden">
-                                <ProjectStatus />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="text-right w-full md:w-1/2 mt-8 md:mt-0 hidden md:flex md:justify-end items-end">
-                        <ProjectStatus />
+                        
                     </div>
                 </div>
             </div>
-        </div>
+        </NavLink>
     )
 }
