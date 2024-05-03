237fdf58c04a81e490d91eab2611ca91f48319db
François Schuers <francois.schuers@gmail.com>
2024-05-03 08:40:41 +0200

    feat: activate feature with env variable

--- app/components/Project/Checkout/Common/PaymentMethod.tsx
+++ app/components/Project/Checkout/Common/PaymentMethod.tsx
@@ -1,7 +1,10 @@
 import { CreditCardIcon, WalletIcon } from "@heroicons/react/24/outline";
+import { useNotifications } from "~/root";
 import { PaymentMethodValues, STRIPE_FEES } from "~/utils/constant";
 
 export default function PaymentMethod({ paymentMethod, setPaymentMethod }: { paymentMethod: PaymentMethodValues, setPaymentMethod: (paymentMethod: PaymentMethodValues) => void}) {
+    const { activateStripePayment } = useNotifications();
+
     return (
         <div className="border rounded-lg border-opacityLight-20">
             <div className="flex justify-between bg-opacityLight-5 py-2 px-3">
@@ -35,13 +38,16 @@ export default function PaymentMethod({ paymentMethod, setPaymentMethod }: { pay
                         checked={paymentMethod === "credit_card"} 
                         onChange={() => setPaymentMethod(PaymentMethodValues.CREDIT_CARD)}
                         className="w-0 h-0 opacity-0 absolute"
+                        disabled={!activateStripePayment}
                     />
                     <label 
                         htmlFor="credit_card" 
-                        className={`flex w-full p-2 items-center justify-start cursor-pointer rounded-lg border border-neutral-600 uppercase text-sm ${paymentMethod === PaymentMethodValues.CREDIT_CARD ? "bg-neutral-100 text-neutral-900" : "bg-opacityLight-5 text-neutral-100 hover:brightness-110"}`}
+                        className={`flex w-full p-2 items-center justify-start rounded-lg border border-neutral-600 uppercase text-sm ${activateStripePayment ? "cursor-pointer" : "cursor-not-allowed brightness-50 hover:brightness-50"} ${paymentMethod === PaymentMethodValues.CREDIT_CARD ? "bg-neutral-100 text-neutral-900" : "bg-opacityLight-5 text-neutral-100 hover:brightness-110"}`}
                     >
                         <CreditCardIcon className="w-6 mr-2" />
-                        Credit Card <span className="text-xs text-neutral-300 normal-case ml-1"> ({STRIPE_FEES}% fees)</span>
+                        Credit Card 
+                        {activateStripePayment && <span className="text-xs text-neutral-300 normal-case ml-1"> ({STRIPE_FEES}% fees)</span>}
+                        {!activateStripePayment && <span className="text-xs text-neutral-300 normal-case ml-1"> (SOON)</span>}
                     </label>
                 </div>
             </div>
